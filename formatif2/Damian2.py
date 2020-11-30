947fcf27a12b5c76274366721e509f1ad7ff78dd
Damian Maiorino <maidam08@ecolecatholique.ca>
2020-11-30 21:16:22 +0000

    added inputs

--- formatif2/Damian2.py
+++ formatif2/Damian2.py
@@ -1,5 +1,9 @@
 
-print("entrer un nombre")
+print("hallo tous le monde")
 user_input = input()
 print(8 * int(user_input) ) # confirmation
 print(8 * 6) # présenter le résultats
+print("Je m'appelle Damian")
+user_input = input()
+print(5 * int(user_input) ) # confirmation
+print(5 * 9) # présenter le résultats
