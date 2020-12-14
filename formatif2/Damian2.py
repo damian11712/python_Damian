504e716202442d0d59bd4afcd7d18123279c284b
Damian Maiorino <maidam08@ecolecatholique.ca>
2020-12-14 23:09:28 +0000

    added inputs, and "commentaires".

--- formatif2/Damian2.py
+++ formatif2/Damian2.py
@@ -1,8 +1,11 @@
 print("hallo tous le monde")
+print("S'ils vous plait mettre n'importe quelle nombre. Cette nombre vas etre multiplier par 8.")
 user_input = input()
 print(8 * int(user_input) ) # confirmation
-print(8 * 6) # présenter le résultats
-print("Je m'appelle Damian")
+# presenter les resultats
+print("Bravo! Tu vois? Je menterai jamais a vous!")
+print("Ok. Maintenant n'importe quelle nombre que tu vas mettre vas etre multiplier par 5!")
 user_input = input()
 print(5 * int(user_input) ) # confirmation
-print(5 * 9) # présenter le résultats
+# presenter les resultats
+print("Merci! ces tous pour aujourd'hui.")
